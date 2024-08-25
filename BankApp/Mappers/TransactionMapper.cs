9043f946854faaaea0feae1d677d42d701b9a516
CaffeineBeast <146594190+CaffeineBeast@users.noreply.github.com>
2024-08-26 00:25:15 +0200

    Transaction mapper now uses appsettings.json to manipulate with xml data

--- BankApp/Mappers/TransactionMapper.cs
+++ BankApp/Mappers/TransactionMapper.cs
@@ -6,19 +6,29 @@ namespace BankApp.Mappers
 {
     public class TransactionMapper : ITransactionMapper
     {
+        private readonly IConfiguration _configuration;
+
+        public TransactionMapper(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
         public async Task<Transaction> MapXmlToModel(XmlNode xmlNode, string fileName)
         {
-            string beforeTrim = xmlNode.Attributes["BrojZaReklamaciju"]?.Value;
-            string pattern = @"\d{19}//";
+            var settings = _configuration.GetSection("TransactionSettings");
+            string pattern = settings["Pattern"];
+            string defaultDebit = settings["DefaultDebit"];
+            var attributes = settings.GetSection("Attributes");
+
+            string beforeTrim = xmlNode.Attributes[attributes["TransactionDate"]]?.Value;
             string afterTrim = Regex.Replace(beforeTrim, pattern, "");
 
             var model = new Transaction
             {
                 TransactionDate = afterTrim,
-                Reference = xmlNode.Attributes["Referenca"]?.Value,
-                Recipient = xmlNode.Attributes["NalogKorisnik"]?.Value,
-                TransactionDescription = xmlNode.Attributes["Opis"]?.Value,
-                Debit = decimal.Parse(xmlNode.Attributes["Duguje"]?.Value ?? "0"),
+                Reference = xmlNode.Attributes[attributes["Reference"]]?.Value,
+                Recipient = xmlNode.Attributes[attributes["Recipient"]]?.Value,
+                TransactionDescription = xmlNode.Attributes[attributes["TransactionDescription"]]?.Value,
+                Debit = decimal.Parse(xmlNode.Attributes[attributes["Debit"]]?.Value ?? defaultDebit),
                 FileName = fileName
             };
 
