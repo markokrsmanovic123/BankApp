8510dae115eda18bd5429aebdface03429e6f788
CaffeineBeast <146594190+CaffeineBeast@users.noreply.github.com>
2024-09-03 23:59:41 +0200

    DataTables is now propertly initialized, Export to Excel, CSV, and Copy functions are now provided by DataTables, Removed unnecessary .js scripts.

--- BankApp/wwwroot/js/InitializeTable.js
+++ BankApp/wwwroot/js/InitializeTable.js
@@ -1,3 +1,33 @@
-﻿$(document).ready(function () {
-    let table = new DataTable('#transactionTable');
+﻿$.fn.dataTable.Buttons.defaults.dom.button.className = 'btn btn-primary';
+
+$(document).ready(function () {
+    let table = new DataTable('#transactionTable', {
+        lengthMenu: [10, 25, 50, 100, { label: 'Show All', value: -1}],
+        layout: {
+            topStart: {
+                buttons: [
+                    'pageLength',
+                    {
+                        extend: 'copy',
+                        title: null
+                    },
+                    {
+                        extend: 'excel',
+                        title: null
+                    },
+                    {
+                        extend: 'csv',
+                        title: null
+                    }
+                ]
+            },
+            topEnd: {
+                search: {
+                    placeholder: 'Search'
+                }
+            }
+        },
+        select: true,
+        responsive: true
+    });
 });
